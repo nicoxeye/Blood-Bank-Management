80fcf86169ead8e3f57df29aec6b122d526794d9
nicoxeye <nicoxeye@op.pl>
2025-05-16 15:43:15 +0200

    initial commit

---
+++ src/test/java/com/app/bloodbank/BloodBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bloodbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
